b16d86e2eaccbf0cc5597c0d384f0d7ef18ad538
Moussa Diagne <musadevweb@medteck.pro>
2023-02-25 01:58:29 +0100

    deploy producion

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, "./out")));
 
-app.use(express.static(
-    path.join(__dirname, './out')
-))
+app.get("/blog", function (req, res) {
+  res.sendFile(path.join(__dirname, "./out", "blog.html"));
+});
 
-app.get('/l',(req, res) => {
-    res.json({message:'Bienvenu'})
-})
-const port = process.env.PORT || 3001
+app.get("/services", function (req, res) {
+  res.sendFile(path.join(__dirname, "./out", "services.html"));
+});
 
-app.listen(port, () => console.log(port))
+app.get("/qui-sommes-nous", function (req, res) {
+  res.sendFile(path.join(__dirname, "./out", "qui-sommes-nous.html"));
+});
+
+app.get("/contact", function (req, res) {
+  res.sendFile(path.join(__dirname, "./out", "contact.html"));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).sendFile(path.join(__dirname, "./out", "404.html"));
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log(port));
